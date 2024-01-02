37b6886c21ef28a4e3476c2eaf19d8c88cca3696
workingtechman <workingtechman@example.com>
2024-01-02 19:03:03 +0300

    added parallel funct

---
+++ vars/runParallelFunc.groovy
@@ -0,0 +1,24 @@
+def call(String name) {
+  stages {
+    stage('test & scan with sonar') { 
+      steps {
+        echo "testing ${name}"
+      }
+    }
+    stage('Build FP_name for ssv') {
+      steps {
+        echo "testing ${name}"
+      }
+    }
+    stage('Build FP_name Docker Image for ssv') {
+      steps {
+        echo "testing ${name}"
+      }
+    }
+    stage('Deploy FP_name to SSV openshift') {
+      steps {
+        echo "testing ${name}"
+      }
+    }
+  }
+}
