df05f438ba446f2b3b126f4fe7b047f0620975aa
workingtechman <workingtechman@example.com>
2024-01-08 18:29:19 +0300

    turn shared function into scripted-like to work in jenkins Parallel function

--- vars/runParallelFunc.groovy
+++ vars/runParallelFunc.groovy
@@ -1,24 +1,27 @@
 def call(String name) {
-//  stages {
-//    stage('test & scan with sonar') { 
-//      steps {
-        echo "testing & scan ${name} with sonar"
-//      }
-//    }
-//    stage('Build FP_name for ssv') {
-//      steps {
-        echo "building ${name}"
-//      }
-//    }
+      return {
+//      stages {
+        node("linux-agent") {
+          stage("${name}") {
+            stage('test & scan with sonar') { 
+              sh "echo Do testing & scan ${name} with sonar"
+            }
+            stage("Build ${name} for ssv") {
+              sh "echo building ${name}"
+            }
+          }
 //    stage('Build FP_name Docker Image for ssv') {
 //      steps {
-        echo "docker build ${name}"
+//        echo "docker build ${name}"
 //      }
 //    }
 //    stage('Deploy FP_name to SSV openshift') {
 //      steps {
-        echo "deploy ${name}"
+//        echo "deploy ${name}"
 //      }
 //    }
 //  }
+        }
+//      }
+      }
 }
